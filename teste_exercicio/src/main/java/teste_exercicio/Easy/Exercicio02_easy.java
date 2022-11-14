cfcf3a5be41f725dfa72b34ad8b228cc95d53ef5
aline-ayrao <alineayrao@gmail.com>
2022-11-13 22:53:18 -0300

    test

--- teste_exercicio/src/main/java/teste_exercicio/Easy/Exercicio02_easy.java
+++ teste_exercicio/src/main/java/teste_exercicio/Easy/Exercicio02_easy.java
@@ -5,6 +5,6 @@ public class Exercicio02_easy { // inicio classe exercicio
 
     public String retornaPalavra(String palavra) {
        
-        return "O Usuario digitou: "+palavra;
+        return palavra;
     } 
 }// fim exercicio
---
+++ teste_exercicio/src/test/java/Teste_Exercicio02_easy.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import teste_exercicio.Easy.Exercicio02_easy;
+
+public class Teste_Exercicio02_easy {
+    @Test
+    public void testRetornaPalavra(){
+        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
+        
+        Assert.assertEquals("Teste", exercicio02_easy.retornaPalavra("Teste"));
+        
+    }
+}
