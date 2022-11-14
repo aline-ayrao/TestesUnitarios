115bed7fcf47fc08c2c417f700918187c9733295
aline-ayrao <alineayrao@gmail.com>
2022-11-14 19:56:30 -0300

    Teste Exercicio 03 Medium

---
+++ teste_exercicio/src/test/java/Medium/Teste_Exercicio03_medium.java
@@ -0,0 +1,21 @@
+package Medium;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import teste_exercicio.Medium.Exercicio03_medium;
+
+public class Teste_Exercicio03_medium {
+    static Exercicio03_medium exercicio03_medium;
+
+    @BeforeClass
+    public static void before(){
+        exercicio03_medium = new Exercicio03_medium();
+    }
+
+    @Test
+    public void testeTabuada(){
+        exercicio03_medium.somaNumerosAcumulados();
+    }
+    
+}
