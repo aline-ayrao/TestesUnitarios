76b9af67b9a434255b947714c8e1b13ac7d7807a
aline-ayrao <alineayrao@gmail.com>
2022-11-13 23:21:08 -0300

    Teste Exercicio03 Easy

---
+++ teste_exercicio/src/test/java/Teste_exercicio03_easy.java
@@ -0,0 +1,17 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import teste_exercicio.Easy.Exercicio03_easy;
+
+public class Teste_Exercicio03_easy {
+    @Test
+    public void testeRetornaValorTrocado(){
+        Exercicio03_easy exercicio03_easy = new Exercicio03_easy();
+
+        String mensagemEsperada = "primeiro valor digitado: segundoValor\nSegundo valor digitado: primeiroValor";
+        String mensagemAtual = exercicio03_easy.retornaValorTrocado("primeiroValor", "segundoValor");
+
+        Assert.assertEquals(mensagemEsperada, mensagemAtual);
+
+    }
+}
